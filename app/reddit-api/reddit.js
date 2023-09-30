1fd7549a7e2e0179e7885e6b2fa007883215369f
Mark Masoumi <masoumi.mark@outlook.com>
2023-09-30 17:43:59 -0400

    implementing app authorization logic for both user auth and app only auth

--- app/reddit-api/reddit.js
+++ app/reddit-api/reddit.js
@@ -2,43 +2,44 @@
 // - Authorizing App via oauth2 grant
 // - Getting Access Token
 // - Fetching Post data
+// Reddit OAuth2 Documentation: https://github.com/reddit-archive/reddit/wiki/OAuth2
+// To search for POSTS use https://www.reddit.com/dev/api/#GET_search and set restrict_sr to FALSE
 
 import axios from "axios";
 
-export function Foo() {
-    console.log(`foo called`);
+// Authorize App + User
+// *User grants this client app permission to access their Reddit profile data
+export function authorizeApp() {
+
+    const params = new URLSearchParams();
+    params.append("client_id", process.env.NEXT_PUBLIC_REDDIT_CLIENT_ID);
+    params.append("response_type", "code");
+    params.append("state", "abc123");
+    params.append("redirect_uri", "http://localhost:3000/app");
+    params.append("duration", 'temporary');
+    params.append("scope", "read");
+
+    document.location = `https://www.reddit.com/api/v1/authorize?${params.toString()}`;
 }
 
 // Authorize App Only
 // *Authorizes this client app without a user context.
 export async function authorizeAppOnly() {
-    console.log(`authorizeAppOnly called`);
-
-    var options = {
+    const options = {
         method: 'POST',
         url: 'https://www.reddit.com/api/v1/access_token',
-        headers: {
-            'content-type': 'application/x-www-form-urlencoded'
-        },
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        auth: { username: process.env.NEXT_PUBLIC_REDDIT_CLIENT_ID, password: process.env.NEXT_PUBLIC_REDDIT_SECRET },
         data: new URLSearchParams({
             grant_type: 'client_credentials'
         })
     };
 
-    axios.request(options)
-        .then(function (response) {
-            console.log(response.data);
-        }).catch(function (error) {
-            console.error(error);
-        });
-
-    // Redirect user too /app after getting the access token
-
-}
-
-// Authorize App + User
-// *User grants this client app permission to access their Reddit profile data
-export function authorizeApp() {
+    axios.request(options).then(function (response) {
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
 
 }
 
