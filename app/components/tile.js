b61c3385f1266f134472e64635e783f1b0be72b9
Mark Masoumi <masoumi.mark@outlook.com>
2023-11-05 12:45:22 -0500

    Removed single padding value that effected all elements in tile and added padding to individual elements. This allows for embeded tweets to fit inside tile on lowest screen resolution (Galaxy Fold: 280 x 653). Next steps are to replace explicit width values with responsive / dynamic values

--- app/components/tile.js
+++ app/components/tile.js
@@ -51,20 +51,20 @@ export default function Tile({ post, loggedIn, userAuthorizeApp, accessToken }) 
         switch (post.mediaType) {
             case "image":
                 // Nextjs method for making images responsive: https://nextjs.org/docs/app/api-reference/components/image#responsive-image-with-aspect-ratio
-                embeddedMedia = <Image src={post.mediaURL} sizes="100vw" style={{ width: '100%', height: 'auto' }} width={500} height={300} alt="" />;
+                embeddedMedia = <Image className="p-3" src={post.mediaURL} sizes="100vw" style={{ width: '100%', height: 'auto' }} width={500} height={300} alt="" />;
                 break;
             case "video":
                 embeddedMedia = <video autoPlay width={320} height={240} controls src={post.mediaURL} type="video/mp4" />;
                 break;
             case "youtube":
                 // Hack for making Youtube videos responsive: https://github.com/CookPete/react-player#responsive-player
-                embeddedMedia = <div style={{ position: 'relative', paddingTop: '56.25%' }}><ReactPlayer style={{ position: 'absolute', top: 0, left: 0 }} width={"100%"} height={"100%"} url={post.mediaURL} /></div>
+                embeddedMedia = <div className="p-3"><div style={{ position: 'relative', paddingTop: '56.25%' }}><ReactPlayer style={{ position: 'absolute', top: 0, left: 0 }} width={"100%"} height={"100%"} url={post.mediaURL} /></div></div>
                 break;
             case "twitter":
                 embeddedMedia = <TwitterTweetEmbed tweetId={post.mediaURL} />
                 break;
             case "link":                
-                embeddedMedia = <><Link className="break-all text-emerald-200 hover:text-emerald-50 font-bold text-center" href={post.mediaURL}>{post.mediaURL}</Link><br/></>
+                embeddedMedia = <div className="px-3"><Link className="break-all text-emerald-200 hover:text-emerald-50 font-bold text-center" href={post.mediaURL}>{post.mediaURL}</Link><br /></div>
                 break;
         }
         return embeddedMedia;
@@ -72,43 +72,43 @@ export default function Tile({ post, loggedIn, userAuthorizeApp, accessToken }) 
 
     return (
         <>
-            <div className="collapse bg-gray-600 w-64 mx-auto mb-5 p-3 hover:outline outline-2 outline-emerald-500">
+            <div className="collapse bg-gray-600 w-64 mx-auto mb-5 hover:outline outline-2 outline-emerald-500">
                 <input type="checkbox" />
                 <div className="collapse-title p-0">
                     {/* Post Title */}
-                    <p className="text-white font-bold text-center text-md mb-2">{post.title}</p>                    
+                    <p className="text-white font-bold text-center text-md mb-2 p-3">{post.title}</p>                    
                     {/* Subreddit */}
-                    <p className="text-yellow-300 font-bold text-sm">r/{post.subreddit}</p>
+                    <p className="text-yellow-300 font-bold text-sm pl-3">r/{post.subreddit}</p>
                     {/* Username */}
-                    <p className="text-emerald-500 font-bold text-sm">u/{post.author}</p>
+                    <p className="text-emerald-500 font-bold text-sm pl-3">u/{post.author}</p>
                     {/* Comment time posted and upvotes */}
-                    <div className="flex flex-row gap-2">
+                    <div className="flex flex-row gap-2 pl-3">
                     <p className="text-lime-500 font-bold text-sm">{post.date}</p>
                     <p className="text-lime-400 font-bold text-sm">{post.upvotes} upvotes</p>
                     </div>
                     <br/>
                     {/* User Comment */}
-                    <div className="user-comment text-white italic text-center" style={{overflowWrap: "anywhere"}}>{post.topCommentText}</div>
+                    <div className="user-comment text-white italic text-center p-3" style={{overflowWrap: "anywhere"}}>{post.topCommentText}</div>
                     {/* Comment icon and Comment username  */}
-                    <div className="flex flex-row gap-1">
+                    <div className="flex flex-row gap-1 pl-3">
                     <div className="bg-comment bg-contain bg-center bg-no-repeat w-5 h-5 pt-6"/>
                     <p className="text-emerald-100 font-bold text-sm">u/{post.topCommentAuthor}</p>
                     </div>
                     {/* Comment time posted and upvotes */}
-                    <div className="flex flex-row gap-2">
+                    <div className="flex flex-row gap-2 pl-3">
                     <p className="text-lime-500 font-bold text-sm">{post.commentDate}</p>
                     <p className="text-lime-400 font-bold text-sm">{post.topCommentUpVotes} upvotes</p>
                     </div>
                 </div>
                 <div className="collapse-content p-0 mt-5">
                     {/* Post content */}
-                    <div className="post-text text-white" style={{overflowWrap: "anywhere"}}>{post.text}</div>
+                    <div className="post-text text-white px-3" style={{overflowWrap: "anywhere"}}>{post.text}</div>
                     {embedPostMedia(post)}
                     <br />
                     {loggedIn ?
                         !post.archived ?
                             <>
-                                <div>
+                                <div className="px-3">
                                     {/* Submit comment button */}
                                     <form onSubmit={handleCommentSubmit}>
                                         {/* Input text field (comment) */}
@@ -120,8 +120,8 @@ export default function Tile({ post, loggedIn, userAuthorizeApp, accessToken }) 
                                     </form>
                                 </div>
                             </>
-                            : <b className="text-white italic text-sm break-normal">Sorry, thread is archived. New comments cannot be posted.</b>
-                        : <><p className="text-sm text-white"><button onClick={userAuthorizeApp} className="font-bold text-emerald-300 hover:text-emerald-50">Log in to Reddit</button> to post comments</p></>}
+                            : <div className="pl-3"><b className="text-white italic text-sm">Sorry, thread is archived. New comments cannot be posted.</b></div>
+                        : <><p className="text-sm text-white pl-3"><button onClick={userAuthorizeApp} className="font-bold text-emerald-300 hover:text-emerald-50">Log in to Reddit</button> to post comments</p></>}
                 </div>
             </div>
         </>
